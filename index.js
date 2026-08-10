e2204b25360897269fefbc3d2bca2e1d83fd000a
Masson <matheusfelipe.masson2007@gmail.com>
2026-08-10 11:07:29 -0300

    Adicionado a rota Update

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.get("/", (request,response) => {
 app.post("/create-filme", (request,response) => {
     const { nomeFilme, gênero, duração, classificaçãoEtária } = request.body
 
-    const insertCommand = "INSERT INTO filmes_MatheusMassonSophiaBatista(nomeFilme, gênero, duração, classificaçãoEtária) VALUES (?, ?, ?, ?)"
+    const insertCommand = "INSERT INTO filmes_MatheusMassonSophiaOliveira(nomeFilme, gênero, duração, classificaçãoEtária) VALUES (?, ?, ?, ?)"
 
     database.query(insertCommand, [nomeFilme, gênero, duração, classificaçãoEtária], (error) => {
         if (error) {
@@ -29,7 +29,7 @@ app.post("/create-filme", (request,response) => {
 app.delete("/delete-filme/:id", (request,response) => {
     const { id } = request.params
 
-    const deleteCommand = "DELETE FROM filmes_MatheusMassonSophiaBatista WHERE id=?"
+    const deleteCommand = "DELETE FROM filmes_MatheusMassonSophiaOliveira WHERE id=?"
 
     database.query(deleteCommand, [id], (error) => {
         if (error) {
@@ -42,34 +42,10 @@ app.delete("/delete-filme/:id", (request,response) => {
     })
 })
 
-app.get("/completed-filme", (request, response) => {
-    const selectCommand = "SELECT * FROM filmes_MatheusMassonSophiaBatista WHERE status = 1"
-
-    database.query(selectCommand, (error, data) => {
-        if (error) {
-            console.log(error)
-        } else {
-            response.json(data)
-        }
-    })
-})
-
-app.get("/incompleted-filme", (request, response) => {
-    const selectCommand = "SELECT * FROM filmes_MatheusMassonSophiaBatista WHERE status = 0"
-
-    database.query(selectCommand, (error, data) => {
-        if (error) {
-            console.log(error)
-        } else {
-            response.json(data)
-        }
-    })
-})
-
 app.put("/update-task/:id", async (req, res) => {
     const { id } = req.params
 
-    const selectTaskCommand = "SELECT * FROM filmes_MatheusMassonSophiaBatista WHERE id = ?"
+    const selectTaskCommand = "SELECT * FROM filmes_MatheusMassonSophiaOliveira id = ?"
 
     const task = await database.promise().query(selectTaskCommand, [id], (error, data) => {
         if (error) {
@@ -78,6 +54,7 @@ app.put("/update-task/:id", async (req, res) => {
             return data
         }
     })
+})
 
 const database = mysql2.createPool({
     host: "benserverplex.ddns.net",
