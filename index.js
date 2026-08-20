d8aceba241cb5054ec62bb09a3158a350d318aa1
Masson <matheusfelipe.masson2007@gmail.com>
2026-08-20 08:22:58 -0300

    Implementação do Frontend

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
-import express, { request, response } from "express";
-import mysql2 from "mysql2";
+import express from "express"
+import mysql2 from "mysql2"
+import cors from "cors"
 
 const app = express()
-app.use(express.json())
 
-app.get("/", (request,response) => {
-    response.json({
-        message: "Você acessou a rota principal"
+app.use(express.json())
+app.use(cors())
+
+app.get("/", (request, response) => {
+    const selectCommand = "SELECT * FROM filmes_MatheusMassonSophiaOliveira"
+
+    database.query(selectCommand, (error, data) => {
+        if (error) {
+            console.log(error)
+        } else {
+            response.json(data)
+        }
     })
 })
 
-app.post("/create-filme", (request,response) => {
+app.post("/create-filme", (request, response) => {
     const { nomeFilme, gênero, duração, classificaçãoEtária } = request.body
 
     const insertCommand = "INSERT INTO filmes_MatheusMassonSophiaOliveira(nomeFilme, gênero, duração, classificaçãoEtária) VALUES (?, ?, ?, ?)"
@@ -26,7 +35,7 @@ app.post("/create-filme", (request,response) => {
     })
 })
 
-app.delete("/delete-filme/:id", (request,response) => {
+app.delete("/delete-filme/:id", (request, response) => {
     const { id } = request.params
 
     const deleteCommand = "DELETE FROM filmes_MatheusMassonSophiaOliveira WHERE id=?"
@@ -36,25 +45,29 @@ app.delete("/delete-filme/:id", (request,response) => {
             console.log(error)
         } else {
             response.json({
-                message: "Filme apagada com sucesso!"
+                message: "Filme apagado com sucesso!"
             })
         }
     })
 })
 
-app.put("/update-task/:id", async (req, res) => {
-    const { id } = req.params
+app.put("/update/:id", (request, response) => {
+    const { id } = request.params
+    const { nomeFilme, gênero, duração, classificaçãoEtária } = request.body
 
-    const selectTaskCommand = "SELECT * FROM filmes_MatheusMassonSophiaOliveira id = ?"
+    const updateCommand = "UPDATE filmes_MatheusMassonSophiaOliveira SET nomeFilme = ?, gênero = ?, duração = ?, classificaçãoEtária = ? WHERE id = ?"
 
-    const task = await database.promise().query(selectTaskCommand, [id], (error, data) => {
+    database.query(updateCommand, [nomeFilme, gênero, duração, classificaçãoEtária, id], (error) => {
         if (error) {
             console.log(error)
         } else {
-            return data
+            response.json({
+                message: "Filme editado com sucesso!"
+            })
         }
     })
 })
+
 
 const database = mysql2.createPool({
     host: "benserverplex.ddns.net",
@@ -64,5 +77,5 @@ const database = mysql2.createPool({
 })
 
 app.listen(3333, () => {
-    console.log("Servider online")
+    console.log("Servidor online")
 })
