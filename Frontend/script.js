10c37c9532e20859aa91540d770402a51dd8f647
Masson <matheusfelipe.masson2007@gmail.com>
2026-09-03 08:35:35 -0300

    Criação da parte cadastrar

--- Frontend/script.js
+++ Frontend/script.js
@@ -11,9 +11,20 @@ async function buscarFilmes() {
                 <p><strong>Gênero:</strong> ${filme.gênero}</p>
                 <p><strong>Duração:</strong> ${filme.duração} minutos</p>
                 <p><strong>Classificação indicativa:</strong> ${filme.classificaçãoEtária > 0 ? filme.classificaçãoEtária + ' anos' : 'Livre'}</p>
+
+                <button onclick="apagarFilme(${filme.id})">Apagar</button>
             </div>
         `
     })
+}
+
+async function apagarFilme(id) {
+    const resposta = await fetch(`https://backend-filmes03mc.vercel.app/delete/${id}`, { method: "DELETE" })
+    const respostaJS = await resposta.json()
+
+    alert(respostaJS.message)
+
+    window.location.reload()
 }
 
 buscarFilmes()
